4b1862aa90c637282126051df418ee2d1d00ccc9
vinithacejojohn <vinithamolkb@gmail.com>
2017-11-26 23:05:27 +1100

    EC2 and S3 implementation

---
+++ site.tf
@@ -0,0 +1,115 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "ec2-status-bucket" {
+  bucket = "ec2-status-bucket"
+  acl    = "private"
+  versioning = 	{
+           enabled = true
+  }
+}
+
+resource "aws_iam_role_policy" "s3_access_policy" {
+  name = "s3_access_policy"
+  role = "${aws_iam_role.s3_access_role.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": ["s3:ListBucket"],
+      "Resource": ["${aws_s3_bucket.ec2-status-bucket.arn}"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject"
+      ],
+      "Resource": ["${aws_s3_bucket.ec2-status-bucket.arn}/*"]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "s3_access_role" {
+  name = "s3_access_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "s3_access_profile" {
+  name = "s3_access_profile"
+  role = "s3_access_role"
+}
+
+
+data "aws_ami" "myimage" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+
+data "template_file" "user_data_template" {
+  template = <<EOF
+#!/bin/bash
+apt-get update
+apt-get install -y python python-pip
+pip install awscli
+
+echo -e "File with instance metadata\n" >> instance_metadata_file.txt
+
+for metadata in {local-hostname,instance-type,ami-id}
+do
+ curl  http://169.254.169.254/latest/meta-data/$metadata >> instance_metadata_file.txt
+ echo ""  >> instance_metadata_file.txt
+done
+aws s3 cp /instance_metadata_file.txt  s3://${aws_s3_bucket.ec2-status-bucket.id}/
+EOF
+
+}
+
+
+resource "aws_instance" "vini_ec2" {
+  instance_type = "t2.micro"
+  key_name = "aws-vinitha"
+  ami = "${data.aws_ami.myimage.image_id}"
+  tags = {
+     Name = "TerraformEC2"
+  }
+  user_data = "${data.template_file.user_data_template.rendered}"
+  iam_instance_profile = "${aws_iam_instance_profile.s3_access_profile.name}"
+
+}
+
+
+output "bucket_arn" {
+  value = "${aws_s3_bucket.ec2-status-bucket.arn}"
+}
