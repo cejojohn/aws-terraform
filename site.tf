acd7224f7545b3d7733b3fc411f7aed6a66146e2
cejojohn <cejokjohn@gmail.com>
2017-12-11 08:31:27 +1100

    Fixing AWS credential variable names

--- site.tf
+++ site.tf
@@ -1,10 +1,10 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
 variable "bucket_name" {}
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = "${var.aws_access_key_id}"
+  secret_key = "${var.aws_secret_access_key}"
   region = "ap-southeast-2"
 }
 
